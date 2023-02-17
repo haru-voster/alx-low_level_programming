c9d6d1535c7e8234483e2089af325faee1bdc256
haru-voster <harounbravo2@gmail.com>
2023-02-17 03:32:07 -0800

    Ngative / positive c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Prints my alphabetical game
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
