1b3342532cf0430bbbf1df76fb8d04a066278153
haru-voster <harounbravo2@gmail.com>
2023-02-17 03:59:51 -0800

    APLbettasks

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Prints the alphabets without q and e
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while
+		(c <= 'z') {
+			if ((c != 'q' && c != 'e') && c <= 'z')
+				putchar(c);
+			c++;
+		}
+	putchar ('\n');
+	return (0);
+}
