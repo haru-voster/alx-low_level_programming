157bce79a648772029f7e345cc84f4133f9621a7
Haroun wabwire <98280456+haru-voster@users.noreply.github.com>
2023-02-19 14:32:49 +0300

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,10 +1,13 @@
 #include <time.h>
 #include <stdio.h>
-/*The number, followed by
-if the number is greater than 0: is positive
-if the number is 0: is zero
-if the number is less than 0: is negative
-followed by a new line*/
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
 	int n;
