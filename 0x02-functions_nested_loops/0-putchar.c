b133034a54255bffca676a1df52b1922ece3e98a
haru-voster <harounbravo2@gmail.com>
2023-02-21 05:51:23 -0800

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/*a c program that prints _putchar*/
+int main()
+{
+    printf("_");
+    printf("p");
+    printf("u");
+    printf("t");
+    printf("c");
+    printf("h");
+    printf("a");
+    printf("r");
+    printf("\n");
+    return 0;
+
+}
