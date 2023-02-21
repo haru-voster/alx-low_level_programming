93bc1eee57010a9f7d6e24a0c9342b1b4f496c40
Haroun wabwire <98280456+haru-voster@users.noreply.github.com>
2023-02-21 18:52:37 +0300

    task10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - prints add two numbers
+ * @i:  int i
+ * @j:  int k
+ *print i and j
+ * Return: Always 0.
+ */
+
+int add(int i, int j)
+{
+	return (i + j);
+}
