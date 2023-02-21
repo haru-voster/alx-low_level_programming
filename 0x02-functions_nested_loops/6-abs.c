3be64d298934d2ba641d24ade0de38056d52e0f5
Haroun wabwire <98280456+haru-voster@users.noreply.github.com>
2023-02-21 18:41:05 +0300

    task6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute value of an integer
+ * @s the interger to be computed
+ * Return: The absolute value
+ */
+
+int _abs(int r)
+{
+	if (r >= 0)
+		return (r);
+	else
+		return (-r);
+}
