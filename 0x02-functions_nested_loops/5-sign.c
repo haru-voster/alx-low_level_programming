cff98d7971272a742747a759c8d47fb966ec7ba8
Haroun wabwire <98280456+haru-voster@users.noreply.github.com>
2023-02-21 19:18:38 +0300

    task5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,7 +2,7 @@
 /**
  * print_sign - Prints the sign of a number.
  * @n: the number of the sign to be printed
- * Returns 1 if the number is greater than zero
+ * Return: always 1 if the number is greater than zero
  * 0 if the num is zero
  * -1 if the num is less than zero
  */
