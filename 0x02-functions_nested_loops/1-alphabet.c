4f604083a43605a93a256aa6740482ed32018fbb
Haroun wabwire <98280456+haru-voster@users.noreply.github.com>
2023-02-21 18:03:13 +0300

    task1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print all alphabet in lower case
+ */
+
+void print_alphabet(void)
+{
+	int j;
+
+	for (j = 'a'; j <= 'z'; j++)
+
+		_putchar(j);
+
+	_putchar('\n');
+}
