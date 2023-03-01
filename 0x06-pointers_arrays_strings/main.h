3ae9d42232fa4cba2d77e7ab62d74a7a8f60f342
Haroun wabwire <98280456+haru-voster@users.noreply.github.com>
2023-03-01 08:22:40 +0300

    main_library

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -2,15 +2,18 @@
 #define MAIN_H
 
 char *_strcat(char *dest, char *src);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *s);
 char *cap_string(char *s);
+char *cap_string(char *);
 char *leet(char *str);
 char *rot13(char *str);
 void print_number(int n);
 int _putchar(char c);
+void print_buffer(char *b, int size);
 
 #endif
