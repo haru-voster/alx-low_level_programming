fad7b614f5e72eb3158c26ddfe0def7c066a081a
Haroun wabwire <98280456+haru-voster@users.noreply.github.com>
2023-03-06 18:43:08 +0300

    POINTER_TO_CHAR

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+void pointer ( char * point) {
+    point = "asd";
+}
+
+int main() {
+    char * point;
+    pointer(point);
+    printf("%s\n", point);
+    return 0;
+}
