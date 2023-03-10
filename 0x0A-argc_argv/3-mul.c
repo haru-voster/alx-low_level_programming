656cd5865dc1b2b092f0a2365a2993e894e58f9e
haru-voster <harounbravo2@gmail.com>
2023-03-10 04:25:47 -0800

    Multiplies Numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - prints buffer in hexa
+ * @argc: he number of command line arguments
+ * @argv: An array containing the program command line arguments.
+ *
+ * Return: Nothing.
+ */
+int main(int argc, char *argv[])
+{
+	int mul = 1;
+
+	if (argc == 3)
+	{
+		mul *= atoi(argv[argc - 2]);
+		mul *= atoi(argv[argc - 1]);
+		printf("%d\n", mul);
+	}
+	else
+		printf("Error\n");
+	return (0);
+
+}
+
