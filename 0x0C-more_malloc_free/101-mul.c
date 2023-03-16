72bfbc078f2741752f7a95ca5d1568e6ffc8f70d
haru-voster <harounbravo2@gmail.com>
2023-03-16 13:41:03 -0700

    Write a program that multiplies two positive numbers.

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,201 +1,149 @@
-#include <stdio.h>
+#include "main.h"
 #include <stdlib.h>
-#include <ctype.h>
+#include <stdio.h>
+
+
 /**
-* _isNull - check if string is nullable
-* @s: string to verify
-* Return: 1 if string is null.
-*/
-int _isNull(char *s)
+ * _print - moves a string one place to the left and prints the string
+ * @str: string to move
+ * @l: size of string
+ *
+ * Return: void
+ */
+void _print(char *str, int l)
 {
-if (*s == '\0')
-{
-return (1);
+	int i, j;
+
+	i = j = 0;
+	while (i < l)
+	{
+		if (str[i] != '0')
+			j = 1;
+		if (j || i == l - 1)
+			_putchar(str[i]);
+		i++;
+	}
+
+	_putchar('\n');
+	free(str);
 }
-if (*s != '0')
-{
-return (0);
-}
-else
-{
-return (_isNull(s + 1));
-}
-}
+
 /**
-* _isNumber - check if string is a number
-* @s: string to verify
-* Return: 1 if string is a number.
-*/
-int _isNumber(char *s)
+ * mul - multiplies a char with a string and places the answer into dest
+ * @n: char to multiply
+ * @num: string to multiply
+ * @num_index: last non NULL index of num
+ * @dest: destination of multiplication
+ * @dest_index: highest index to start addition
+ *
+ * Return: pointer to dest, or NULL on failure
+ */
+char *mul(char n, char *num, int num_index, char *dest, int dest_index)
 {
-if (*s == '\0')
-{
-return (1);
-}
-if (!isdigit(*s))
-{
-return (0);
-}
-else
-{
-return (_isNumber(s + 1));
-}
-}
-/**
-* _strlen_recursion - return the length of a string
-* @s: char pointer
-* Return: the length of a string
-*/
-int _strlen_recursion(char *s)
-{
-if (*s != '\0')
-{
-return (_strlen_recursion(s + 1) + 1);
-}
-else
-{
-return (0);
-}
-}
-/**
-* _memset - fills the first n bytes of the memory area pointed to by s
-*           with the constant byte b
-* @s: pointer to memory area
-* @b: constant
-* @n: number ot bytes to fill
-* Return: write n bytes of value b
-*/
-int *_memset(int *s, int b, unsigned int n)
-{
-unsigned int cLoop;
-for (cLoop = 0; cLoop < n; cLoop++)
-{
-s[cLoop] = b;
-}
-return (s);
+	int j, k, mul, mulrem, add, addrem;
+
+	mulrem = addrem = 0;
+	for (j = num_index, k = dest_index; j >= 0; j--, k--)
+	{
+		mul = (n - '0') * (num[j] - '0') + mulrem;
+		mulrem = mul / 10;
+		add = (dest[k] - '0') + (mul % 10) + addrem;
+		addrem = add / 10;
+		dest[k] = add % 10 + '0';
+	}
+	for (addrem += mulrem; k >= 0 && addrem; k--)
+	{
+		add = (dest[k] - '0') + addrem;
+		addrem = add / 10;
+		dest[k] = add % 10 + '0';
+	}
+	if (addrem)
+	{
+		return (NULL);
+	}
+	return (dest);
 }
 /**
-* _intermediate_multiply - multiplication by the number 2
-* @prmResult: array of the current result
-* @prmCLoop: current index loop of the number 1
-* @prmDigit: current digit to multiply
-* @prmNum: Number 2 to multiply
-* Return: integer number
-*/
-int *_intermediate_multiply(
-int *prmResult,
-int prmCLoop,
-int prmDigit,
-char *prmNum
-) {
-int carry = 0, cLoop2 = 0, size = _strlen_recursion(prmNum), indexNumber;
-int digit, sum = 0;
-for (indexNumber = size - 1; indexNumber >= 0; indexNumber--)
+ * check_for_digits - checks the arguments to ensure they are digits
+ * @av: pointer to arguments
+ *
+ * Return: 0 if digits, 1 if not
+ */
+int check_for_digits(char **av)
 {
-digit = prmNum[indexNumber] - 48;
-sum = prmDigit * digit + prmResult[prmCLoop + cLoop2] + carry;
-carry = sum / 10;
-prmResult[prmCLoop + cLoop2] = sum % 10;
-cLoop2++;
+	int i, j;
+
+	for (i = 1; i < 3; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+		{
+			if (av[i][j] < '0' || av[i][j] > '9')
+				return (1);
+		}
+	}
+	return (0);
 }
-if (carry > 0)
-{
-prmResult[prmCLoop + cLoop2] += carry;
-}
-return (prmResult);
-}
+
 /**
-* _revInt - create a reverted array of the result
-* @prmNum: array result
-* @prmSize: size of the result
-* Return: character array
-*/
-char *_revInt(int *prmNum, int prmSize)
+ * init - initializes a string
+ * @str: sting to initialize
+ * @l: length of strinf
+ *
+ * Return: void
+ */
+void init(char *str, int l)
 {
-int digit, indexNumber1, indexNumber2;
-char *s;
-indexNumber1 = prmSize - 1;
-s = malloc(sizeof(char) * (prmSize + 1));
-for (indexNumber2 = 0; indexNumber2 < prmSize; indexNumber2++)
-{
-digit = prmNum[indexNumber1 - indexNumber2];
-s[indexNumber2] = digit + 48;
+	int i;
+
+	for (i = 0; i < l; i++)
+		str[i] = '0';
+	str[i] = '\0';
 }
-s[prmSize] = '\0';
-return (s);
-}
+
 /**
-* _multiply - multiplies two positive numbers.
-* @prmNum1: char number 1
-* @prmNum2: char number 2
-* Return: result.
-*/
-char *_multiply(char *prmNum1, char *prmNum2)
-{
-int size1 = _strlen_recursion(prmNum1);
-int size2 = _strlen_recursion(prmNum2);
-int size = size1 + size2;
-int *result = malloc(sizeof(int) * size);
-int cLoop1 = 0, indexNumber1, digit1;
-char *s;
-if (result == NULL)
-{
-return (NULL);
-}
-if (size1 == 0 || size2 == 0)
-{
-return (NULL);
-}
-result = _memset(result, 0, size1 + size2);
-for (indexNumber1 = size1 - 1; indexNumber1 >= 0; indexNumber1--)
-{
-digit1 = prmNum1[indexNumber1] - 48;
-result = _intermediate_multiply(result, cLoop1, digit1, prmNum2);
-cLoop1++;
-}
-for (
-indexNumber1 = size - 1;
-indexNumber1 >= 0 && result[indexNumber1] == 0;
-indexNumber1--
-) {
-if (result[indexNumber1] != 0)
-break;
-}
-if (indexNumber1 == -1)
-{
-return (NULL);
-}
-size = indexNumber1 + 1;
-s = _revInt(result, indexNumber1 + 1);
-return (s);
-}
-/**
-* main - check the code for Holberton School students.
-* @argc: number of arguments
-* @argv: argument's array
-* Return: Always 0.
-*/
+ * main - multiply two numbers
+ * @argc: number of arguments
+ * @argv: argument vector
+ *
+ * Return: zero, or exit status of 98 if failure
+ */
 int main(int argc, char *argv[])
 {
-int cLoop, hasNullableArgument;
-if (argc != 3)
-{
-printf("Error\n");
-return (98);
-}
-for (cLoop = 1; cLoop < argc; cLoop++)
-{
-if (!_isNumber(argv[cLoop]))
-{
-printf("Error\n");
-exit(98);
-}
-if (!hasNullableArgument && _isNull(argv[cLoop]))
-hasNullableArgument = 1;
-}
-if (hasNullableArgument)
-printf("%s\n", "0");
-else
-printf("%s\n", _multiply(argv[1], argv[2]));
-return (0);
+	int l1, l2, ln, ti, i;
+	char *a;
+	char *t;
+	char e[] = "Error\n";
+
+	if (argc != 3 || check_for_digits(argv))
+	{
+		for (ti = 0; e[ti]; ti++)
+			_putchar(e[ti]);
+		exit(98);
+	}
+	for (l1 = 0; argv[1][l1]; l1++)
+		;
+	for (l2 = 0; argv[2][l2]; l2++)
+		;
+	ln = l1 + l2 + 1;
+	a = malloc(ln * sizeof(char));
+	if (a == NULL)
+	{
+		for (ti = 0; e[ti]; ti++)
+			_putchar(e[ti]);
+		exit(98);
+	}
+	init(a, ln - 1);
+	for (ti = l2 - 1, i = 0; ti >= 0; ti--, i++)
+	{
+		t = mul(argv[2][ti], argv[1], l1 - 1, a, (ln - 2) - i);
+		if (t == NULL)
+		{
+			for (ti = 0; e[ti]; ti++)
+				_putchar(e[ti]);
+			free(a);
+			exit(98);
+		}
+	}
+	_print(a, ln - 1);
+	return (0);
 }
