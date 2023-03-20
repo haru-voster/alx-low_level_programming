078cf1915d771fa434c783c8d37cf2d21a050f9b
haru-voster <harounbravo2@gmail.com>
2023-03-20 14:15:12 -0700

    prints new dogs

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -22,7 +22,7 @@ if (doge == NULL)
 free(doge);
 return (NULL);
 }
-doge->name = malloc(i * sizeof(doge->name));
+doge->name = malloc(i *sizeof(doge->name));
 if (doge->name == NULL)
 {
 free(doge->name);
@@ -32,7 +32,7 @@ return (NULL);
 for (k = 0; k <= i; k++)
 doge->name[k] = name[k];
 doge->age = age;
-doge->owner = malloc(j * sizeof(doge->owner));
+doge->owner = malloc(j *sizeof(doge->owner));
 if (doge->owner == NULL)
 {
 free(doge->owner);
@@ -44,3 +44,4 @@ for (k = 0; k <= j; k++)
 doge->owner[k] = owner[k];
 return (doge);
 }
+
