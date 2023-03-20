8edfe8acbdb50b437c3558d6698897c3daddd7a5
haru-voster <harounbravo2@gmail.com>
2023-03-20 05:47:53 -0700

    compue absolute value of a number x.

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,10 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+#define ABS(x) ((x < (0)) ? ((x) * (-1)) : (x))
+#endif
