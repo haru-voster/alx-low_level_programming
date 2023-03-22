9abc2803f296007e7d37590d1780b5a931a4e995
haru-voster <harounbravo2@gmail.com>
2023-03-22 08:09:27 -0700

    simple operation

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,51 +1,35 @@
-#include <stdio.h>
-#include "function_pointers.h"
+#include "3-calc.h"
 #include <stdlib.h>
-#include <string.h>
-
+#include <stdio.h>
 /**
- * main - check the program
- * @argc: arguments cant
- * @argv: vector arguments
- * Return: 0 if false, something else otherwise.
- */
-int main(int argc, char *argv[])
+* main - Prints the result of simple operations.
+* @argc: The number of arguments supplied to the program.
+* @argv: An array of pointers to the arguments.
+* Return: Always 0.
+*/
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-	int (*get)(int, int);
-	int a, b, operator;
-
-
-	if (argc != 4)
-	{
-		printf("Error\n");
-		exit(98);
-	}
-	if (strlen(argv[2]) != 1)
-	{
-		printf("Error\n");
-		exit(99);
-	}
-	operator = argv[2][0];
-	switch (operator)
-	{
-	case '+':
-		break;
-	case '-':
-		break;
-	case '*':
-		break;
-	case '/':
-		break;
-	case '%':
-		break;
-	default:
-		printf("Error\n");
-		exit(99);
-	}
-
-	a = atoi(argv[1]);
-	b = atoi(argv[3]);
-	get = get_op_func(argv[2]);
-	printf("%d\n", get(a, b));
-	return (0);
+int num1, num2;
+char *op;
+if (argc != 4)
+{
+printf("Error\n");
+exit(98);
+}
+num1 = atoi(argv[1]);
+op = argv[2];
+num2 = atoi(argv[3]);
+if (get_op_func(op) == NULL || op[1] != '\0')
+{
+printf("Error\n");
+exit(99);
+}
+if ((*op == '/' && num2 == 0) ||
+(*op == '%' && num2 == 0))
+{
+printf("Error\n");
+exit(100);
+}
+printf("%d\n", get_op_func(op)(num1, num2));
+return (0);
 }
