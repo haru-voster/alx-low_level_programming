938a1c1ea58b8bd3753d4961c0e3093607842136
haru-voster <harounbravo2@gmail.com>
2023-03-22 09:04:30 -0700

    LIBRARY_PROTOTYPE

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -3,26 +3,10 @@
 
 #include <stdlib.h>	
 #include<stdio.h>
-/* * put char that prints 
- * on error return -1
- * */
-int _putchar(char c)
-{
-	return( write(1, &c, 1));
-=======
-#include <unistd.h>
-/**
- * * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
+#include <stddef.h>
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 int (*get_op_func(char *s))(int, int);
+int _putchar(char c);
 #endif 
