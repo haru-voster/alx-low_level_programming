b45655d8013f2aeef81f589ec0c2f0222c87aed9
haru-voster <harounbravo2@gmail.com>
2023-03-22 09:21:06 -0700

    prints name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,15 +1,15 @@
 #include "function_pointers.h"
-
 #include <stdio.h>
 
 /**
- * print_name - check the code for ALX School students.
+ * print_name - print name using pointer to a function.
  * @name: name of a person.
  * @f: print name as is and UPPERCASE.
  * Return: Always 0.
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name != NULL && f != NULL)
-		f(name);
+  if (name == NULL || f == NULL)
+   return;
+    f(name);
 }
