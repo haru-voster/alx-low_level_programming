3cbeed2e5edf7f70fe7f4b01d212cf7ef705fe03
haru-voster <harounbravo2@gmail.com>
2023-03-23 07:12:37 -0700

    LIB

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,10 +1,9 @@
-#ifndef FUNCTION_POINTERS_H
-#define FUNCTION_POINTERS_H
-#include <stdlib.h>
-#include<stdio.h>
-#include <stddef.h>
+#ifndef FUNCTION_POINTERS
+#define FUNCTION_POINTERS
+
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
-int int_index(int *array, int size, int(*cmp)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+int (*get_op_func(char *s))(int, int);
 int _putchar(char c);
-#endif 
+#endif
