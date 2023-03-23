15742087c959e582d8f5567c57be34319bd60660
haru-voster <harounbravo2@gmail.com>
2023-03-23 06:41:59 -0700

    Print name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,12 +2,12 @@
 
 /**
 *print_name - function that print a name
-*@name: name 
+*@name: name
 *@f: pointer to function
 */
 void print_name(char *name, void (*f)(char *))
 {
 if (name == NULL || f == NULL)
-return;
+	return;
 f(name);
 }
