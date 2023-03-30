d4dc7c238c6d348d4af858850707f3e58acf63b3
haru-voster <harounbravo2@gmail.com>
2023-03-30 03:09:15 -0700

    adds a new node at the end of a list_t list.

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,41 +1,50 @@
-#include "lists.h"
-#include <stdlib.h>
-#include <string.h>
-
-typedef struct list_s {
-    char *str;
-    struct list_s *next;
-} list_t;
-
-list_t *add_node_end(list_t **head, const char *str) {
-    if (str == NULL) {
-        return NULL;
-    }
-
+#include "list.h"
+#include <stdio.h>
+/**
+ * add_node_end - Adds a new node at the end of a list_t list
+ * @head: Pointer to the head of the list
+ * @str: String to be added to the new node
+ *
+ * Return: Address of the new element, or NULL on failure
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
     list_t *new_node = malloc(sizeof(list_t));
-    if (new_node == NULL) {
-        return NULL;
-    }
+    list_t *last_node = *head;
 
-    char *str_copy = strdup(str);
-    if (str_copy == NULL) {
-        free(new_node);
-        return NULL;
-    }
+    if (new_node == NULL)
+        return (NULL);
 
-    new_node->str = str_copy;
+    new_node->str = strdup(str);
+    new_node->len = _strlen(str);
     new_node->next = NULL;
 
     if (*head == NULL) {
         *head = new_node;
-    } else {
-        list_t *current_node = *head;
-        while (current_node->next != NULL) {
-            current_node = current_node->next;
-        }
-        current_node->next = new_node;
+        return (new_node);
     }
 
-    return new_node;
+    while (last_node->next != NULL)
+        last_node = last_node->next;
+
+    last_node->next = new_node;
+
+    return (new_node);
+}
+
+/**
+ * _strlen - Returns the length of a string
+ * @str: String to be measured
+ *
+ * Return: Length of the string
+ */
+int _strlen(const char *str)
+{
+    int len = 0;
+
+    while (str[len] != '\0')
+        len++;
+
+    return (len);
 }
 
