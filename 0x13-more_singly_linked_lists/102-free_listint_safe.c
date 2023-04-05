9a19906b8c8f638138546e9660b8eb35da6e1d32
haru-voster <harounbravo2@gmail.com>
2023-04-05 00:45:15 -0700

    ree listint

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,101 +1,49 @@
-/*
- * File: 102-free_listint_safe.c
- * Auth: Brennan D Baraban
- */
-
 #include "lists.h"
-
-size_t looped_listint_count(listint_t *head);
-size_t free_listint_safe(listint_t **h);
+#include <stdlib.h>
 
 /**
- * looped_listint_count - Counts the number of unique nodes
- *                      in a looped listint_t linked list.
- * @head: A pointer to the head of the listint_t to check.
- *
- * Return: If the list is not looped - 0.
- *         Otherwise - the number of unique nodes in the list.
+ * free_listint_safe - function that free a listint_t linked list
+ * @h: pointer to the beginning of linked list
+ * Return: the number of nodes in the list
  */
-size_t looped_listint_count(listint_t *head)
-{
-	listint_t *tortoise, *hare;
-	size_t nodes = 1;
 
-	if (head == NULL || head->next == NULL)
-		return (0);
-
-	tortoise = head->next;
-	hare = (head->next)->next;
-
-	while (hare)
-	{
-		if (tortoise == hare)
-		{
-			tortoise = head;
-			while (tortoise != hare)
-			{
-				nodes++;
-				tortoise = tortoise->next;
-				hare = hare->next;
-			}
-
-			tortoise = tortoise->next;
-			while (tortoise != hare)
-			{
-				nodes++;
-				tortoise = tortoise->next;
-			}
-
-			return (nodes);
-		}
-
-		tortoise = tortoise->next;
-		hare = (hare->next)->next;
-	}
-
-	return (0);
-}
-
-/**
- * free_listint_safe - Frees a listint_t list safely (ie.
- *                     can free lists containing loops)
- * @h: A pointer to the address of
- *     the head of the listint_t list.
- *
- * Return: The size of the list that was freed.
- *
- * Description: The function sets the head to NULL.
- */
 size_t free_listint_safe(listint_t **h)
 {
-	listint_t *tmp;
-	size_t nodes, index;
+	int i, flag = 0;
+	listint_t *slow, *fast, *delete;
 
-	nodes = looped_listint_count(*h);
+	if (!h)
+		return (0);
 
-	if (nodes == 0)
+	for (i = 0; *h && !flag; i++)
 	{
-		for (; h != NULL && *h != NULL; nodes++)
+		slow = *h;
+		fast = (**h).next;
+		while (slow != fast)
 		{
-			tmp = (*h)->next;
-			free(*h);
-			*h = tmp;
+			if (slow)
+				slow = (*slow).next;
+			if (fast)
+				fast = (*fast).next;
+			if (fast == *h)
+				flag = 1;
+			if (fast)
+				fast = (*fast).next;
+			if (fast == *h)
+				flag = 1;
 		}
+		delete = *h;
+		*h = (**h).next;
+		free(delete);
 	}
 
-	else
+	while (flag && *h != fast)
 	{
-		for (index = 0; index < nodes; index++)
-		{
-			tmp = (*h)->next;
-			free(*h);
-			*h = tmp;
-		}
-
-		*h = NULL;
+		delete = *h;
+		i++;
+		*h = (**h).next;
+		free(delete);
 	}
-
-	h = NULL;
-
-	return (nodes);
+	*h = NULL;
+	return (i);
 }
