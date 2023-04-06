be8d4f347a397e110b2897101b1bae6204ed865c
haru-voster <harounbravo2@gmail.com>
2023-04-06 07:32:15 -0700

    number of bit to flip

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,22 +1,28 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
-  * flip_bits - count bits you need to flip to get from one number to another
-  * @n: first number
-  * @m: second number
-  * Return: number of flips to get from one number to the other
-  */
-
+ * flip_bits - Write a function that returns the number of bits you would
+ * need to flip to get from one number to another.
+ * Prototype: unsigned int flip_bits(unsigned long int n, unsigned long int m);
+ * You are not allowed to use the % or / operators
+ * @n: Number Input 1.
+ * @m: Number Input 2.
+ * Return: Number of flip in the Input numbers.
+ */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
+
 {
-	int count = 0, check;
+	unsigned int flip = 0, cmp, comp1;
 
-	check = (n ^ m);
-	while (check)
+	while (!(n == 0 && m == 0))
 	{
-		count += (check & 1);
-		check >>= 1;
+		cmp = n & 1;
+		comp1 = m & 1;
+		n = n >> 1;
+		m = m >> 1;
+		if (cmp != comp1)
+			flip += 1;
 	}
-	return (count);
-
+	return (flip);
 }
