02fa608e0113a5d428df9291f779971e9d18d1a8
haru-voster <harounbravo2@gmail.com>
2023-04-09 11:05:51 -0700

    Library prototype

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -3,8 +3,11 @@
 #include <stdio.h>
 #include <stddef.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <fcntl.h>
 void copy_text_file(int f1, int f2, char *file_1, char *file_2);
+void check_elf(unsigned char *e_iden_t)
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
