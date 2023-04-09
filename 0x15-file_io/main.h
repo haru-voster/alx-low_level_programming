1107f4f5e9087921e0b546a9ed253081e4a6b363
haru-voster <harounbravo2@gmail.com>
2023-04-09 10:07:03 -0700

    textfile

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,9 +5,15 @@
  * @haroun
  *github.com/haru-voster
  */
+#include <stddef.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void copy_textfile(char *filename, char *filecopy, int letters);
+int create_copyfile(const char *filename, char *text_content);
+int append_text_to_copyfile(const char *filename, char *text_content);
 
 #endif
