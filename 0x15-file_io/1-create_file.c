92abadd9881d3840d4e7bc200a565739d5da3d5d
haru-voster <harounbravo2@gmail.com>
2023-04-09 10:13:52 -0700

    creates a file named _file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,34 @@
+#inlude "main.h"
+/**
+  * create_file - creates a file or truncates it
+  * @filename: the name of the file to create
+  * @text_content: NULL terminated string to write to file
+  * Return: 1 on succes, -1 on failue
+  */
+
+int create_file(const char *filename, char *text_content)
+{
+	int _file, len = 0, check = 0;
+
+	if (!filename)
+		return (-1);
+
+	/* creates file or truncates a file, and give it permission -rw------ */
+	file = open(filename, O_RDWR | O_TRUNC | O_CREAT, 0600);
+	if (_file == -1)
+		return (-1);
+
+	/* if text, writes text into file */
+	if (text_content)
+	{
+		for (len = 0; text_content[len]; len++)
+			;
+		check = write(_file, text_content, len);
+	}
+	/* closes file */
+	close(_file);
+	if (check != len)
+		return (-1);
+
+	return (1);
+}
