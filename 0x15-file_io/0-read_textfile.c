31c5195fe0822033f6cbe6f820d44a06bcbe9763
haru-voster <harounbravo2@gmail.com>
2023-04-11 07:24:11 -0700

    text file and prints it to the POSIX standard output.

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,31 +1,32 @@
-/*
- * File: 0-read_textfile.c
- * Haroun MAGIO
-*/
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <fcntl.h>
 
-ssize_t read_file(const char *filename, size_t letters) {
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+ssize_t read_textfile(const char *filename, size_t letters) {
     if (filename == NULL) {
         return 0; // Return 0 if filename is NULL
     }
 
-    int text = open(filename, O_RDONLY);
-    if (text == -1) {
+    int file1 = open(filename, O_RDONLY);
+    if (file1 == -1) {
         return 0; // Return 0 if file can't be opened
     }
 
     char buffer[letters];
-    ssize_t bytes_read = read(text, buffer, letters);
+    ssize_t bytes_read = read(file1, buffer, letters);
     if (bytes_read == -1) {
-        close(text);
+        close(file1);
         return 0; // Return 0 if read fails
     }
 
     ssize_t bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-    close(text);
+    close(file1);
 
     if (bytes_written == -1 || bytes_written != bytes_read) {
         return 0; // Return 0 if write fails or doesn't write expected amount of bytes
