e9717897b17c63e575b80f35a9ead259ff728fbc
haru-voster <harounbravo2@gmail.com>
2023-04-11 07:20:10 -0700

    text file and prints it to the POSIX standard output.

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,44 +1,36 @@
 /*
  * File: 0-read_textfile.c
- * Haroun mAGIO
- */
-
+ * Haroun MAGIO
+*/
 #include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
-/**
- * read_textfile - Reads a text file and prints it to POSIX stdout.
- * @filename: A pointer to the name of the file.
- * @letters: The number of letters the
- *           function should read and print.
- *
- * Return: If the function fails or filename is NULL - 0.
- *         O/w - the actual number of bytes the function can read and print.
- */
-ssize_t read_textfile(const char *filename, size_t letters)
-{
-	ssize_t o, r, w;
-	char *buffer;
+ssize_t read_file(const char *filename, size_t letters) {
+    if (filename == NULL) {
+        return 0; // Return 0 if filename is NULL
+    }
 
-	if (filename == NULL)
-		return (0);
+    int text = open(filename, O_RDONLY);
+    if (text == -1) {
+        return 0; // Return 0 if file can't be opened
+    }
 
-	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
-		return (0);
+    char buffer[letters];
+    ssize_t bytes_read = read(text, buffer, letters);
+    if (bytes_read == -1) {
+        close(text);
+        return 0; // Return 0 if read fails
+    }
 
-	o = open(filename, O_RDONLY);
-	r = read(o, buffer, letters);
-	w = write(STDOUT_FILENO, buffer, r);
+    ssize_t bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+    close(text);
 
-	if (o == -1 || r == -1 || w == -1 || w != r)
-	{
-		free(buffer);
-		return (0);
-	}
+    if (bytes_written == -1 || bytes_written != bytes_read) {
+        return 0; // Return 0 if write fails or doesn't write expected amount of bytes
+    }
 
-	free(buffer);
-	close(o);
-
-	return (w);
+    return bytes_written; // Return the actual number of letters read and printed
 }
+
