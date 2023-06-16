fbaa59fc93d4e6a1bbc11c1adc0092f3b7ddca20
Haroun wabwire <98280456+haru-voster@users.noreply.github.com>
2023-06-16 04:32:39 +0300

    add node at the beginning of a dlistint_t list.

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -17,25 +17,9 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	if (next == NULL)
 		return (NULL);
 
-<<<<<<< HEAD
-	if (!head || !(*head))
-	{
-		*head = new_node;
-		return (new_node);
-	}
-	else
-	{
-		temp = *head;
-		temp->prev = new_node;
-		*head = new_node;
-		new_node->next = temp;
-		return (new_node);
-	}
-=======
 	next->n = n;
 	next->next = *head;
 	next->prev = NULL;
->>>>>>> d01017ae46aa4b7214c8f73b3562c2b99fc0b573
 
 	if (*head != NULL)
 		(*head)->prev = next;
