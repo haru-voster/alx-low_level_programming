259b752c729c69176b4508b0acdd25a139b8f73e
Haroun wabwire <98280456+haru-voster@users.noreply.github.com>
2023-06-16 04:17:26 +0300

    add node to DLL

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,55 +1,28 @@
 #include "lists.h"
-#include <stdlib.h>
 
 /**
- * create_node - creates a new DLL node with data
- * @n: data to add to node
- *
- * Return: pointer to newly allocated/populated node
- */
-dlistint_t *create_node(const int n)
-{
-	dlistint_t *new_node = NULL;
-
-	new_node = malloc(sizeof(dlistint_t));
-	if (!new_node)
-		return (NULL);
-
-	new_node->next = NULL;
-	new_node->prev = NULL;
-	new_node->n = n;
-
-	return (new_node);
-}
-
-/**
- * add_dnodeint - adds a node to the head of a doubly linked list
- * @head: double pointer to the head, so we can modify if needed
- * @n: data to add to new node
- *
- * Return: pointer to new element, NULL on failure.
+ * add_dnodeint - add node at beginning of the list
+ * @head: double ptr to the head of the list
+ * @n:data
+ * Return: next pointer
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *new_node = NULL, *temp = NULL;
+	dlistint_t *next;
 
-	new_node = create_node(n);
-	if (!new_node)
+	if (head == NULL)
+		return (NULL);
+	next = malloc(sizeof(dlistint_t));
+
+	if (next == NULL)
 		return (NULL);
 
-	if (!head || !(*head))
-	{
-		*head = new_node;
-		return (new_node);
-	}
-	else 
-	{
-		temp = *head;
-		temp->prev = new_node;
-		*head = new_node;
-		new_node->next = temp;
-		return (new_node);
-	}
+	next->n = n;
+	next->next = *head;
+	next->prev = NULL;
 
-	return (NULL);
+	if (*head != NULL)
+		(*head)->prev = next;
+	*head = next;
+	return (next);
 }
