65d89d8b6de8a73e95b19830d61aad720b113b79
Haroun wabwire <98280456+haru-voster@users.noreply.github.com>
2023-07-07 20:09:54 +0300

    prints hash table

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - prints key/value pairs in hash table array order.
+ * @ht: the hash table.
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int b, count = 0;
+	hash_node_t *node;
+
+	if (ht == NULL)
+		return;
+	printf("{");
+	for (h = 0; h < ht->size; h++)
+	{
+		if (ht->array[h] != NULL)
+		{
+			node = ht->array[b];
+			while (node != NULL)
+			{
+				if (count > 0)
+					printf(", ");
+				printf("'%s': '%s'", node->key, node->value);
+				node = node->next;
+				count++;
+			}
+		}
+	}
+	printf("}\n");
+}
