4e45c43eecc3995c6a4a117b9d468b41e56cbe22
Haroun wabwire <98280456+haru-voster@users.noreply.github.com>
2023-07-11 17:35:40 +0300

    Create a rectangle

---
+++ 0x1A-hash_tables/8-rectangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+"""
+8-rectangle.py
+haru-voster
+"""
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle class that inherits from BaseGeometry"""
+
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
