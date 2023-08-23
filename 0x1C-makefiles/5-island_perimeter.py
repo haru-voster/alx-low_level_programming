03caade68f240e83ae3604c3dafdc98e251723b4
Haroun wabwire <98280456+haru-voster@users.noreply.github.com>
2023-08-23 21:27:26 +0300

    island perimeter

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+# haroun Magio
+"""iskand perimeter module """
+
+
+def island_perimeter(grid):
+    """Returns the perimeter of the island described in grid"""
+    ptr = 0
+    length = len(grid) - 1
+    width = len(grid[0]) - 1
+
+    for i, r in enumerate(grid):
+        for j, n in enumerate(r):
+            if n == 1:
+                if i == 0 or grid[i - 1][j] != 1:
+                    ptr += 1
+                if j == 0 or grid[i][j - 1] != 1:
+                    ptr += 1
+                if j == width or grid[i][j + 1] != 1:
+                    ptr += 1
+                if i == length or grid[i + 1][j] != 1:
+                    ptr += 1
+    return ptr
